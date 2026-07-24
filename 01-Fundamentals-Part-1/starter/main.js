8ba8043897ad40c817d1a5dc045e3c434b2db326
AlyssaBaaroun <alyssa.baaroun@student.hepl.be>
2026-07-25 01:03:18 +0200

    feat: create and link main.js file and starting exercises

---
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -0,0 +1,48 @@
+/*
+test debut de chapitre
+
+let js = 'amazing';
+
+if (js === 'amazing') {
+    alert('javaScript is fun');
+} else {
+    alert('javascript is boring');
+}*/
+/*
+Values and Variables
+
+- Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+- Log their values to the console.
+
+let country = 'Belgium';
+let continent = 'Europe';
+let population = 11000000;
+
+
+console.log(`my country is ${country}, it is on the continent of ${continent} and it have around ${population} people`);*/
+
+
+/*
+Data primitive = 7 number, string, undefined, null, boolen, bigint, symbol
+
+- Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+- Log the types of isIsland, population, country and language to the console.
+
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+let country = 'Belgium';
+let continent = 'Europe';
+let population = 11000000;
+
+let isIsland = true;
+let language;
+console.log(`my country is ${country}, it is on the continent of ${continent} and it have around ${population} people. the Island is ${isIsland} and the language is ${language}`);
+console.log(typeof language);
+console.log(typeof isIsland);
+
+*/
+
+
